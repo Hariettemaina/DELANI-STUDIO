e56a6145ab0d79151f0ef565a94a18b765c8b6b1
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-03-14 09:33:54 +0300

    added function that alerts

--- main.js
+++ main.js
@@ -100,5 +100,24 @@ $(document).ready(function(){
     });
 })
 
+$("#submit").click(function(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if(name == ""){
+      alert("enter your name")
+    } 
+    else if(email == ""){
+      alert("enter your email")
+    }
+    else if(message == ""){
+      alert("write a message")
+    }
+    else {
+     alert(name + " " + "we have received your message. Thank you for reaching out to us.")
+    }
+  })
+
 
 
