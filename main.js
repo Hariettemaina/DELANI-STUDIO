b2a4780de644e5f47b40f2d4c71a7a43b4144952
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-03-14 03:48:36 +0300

    added funvtions for mouse hover

--- main.js
+++ main.js
@@ -20,6 +20,85 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+    $('#overlay4').mouseenter(function(){
+        $('.portfolio-text').filter('#wrk4').show('slow')
+    }).mouseleave(function() {   
+    $('#wrk4').hide('slow');
+   
+       
+    });
+})
+
+$(document).ready(function(){
+    $('#overlay3').mouseenter(function(){
+        $('.portfolio-text').filter('#wrk3').show('slow')
+    }).mouseleave(function() {   
+    $('#wrk3').hide('slow');
+   
+       
+    });
+})
+
+$(document).ready(function(){
+    $('#overlay2').mouseenter(function(){
+        $('.portfolio-text').filter('#wrk2').show('slow')
+    }).mouseleave(function() {   
+    $('#wrk2').hide('slow');
+   
+       
+    });
+})
+
+$(document).ready(function(){
+    $('#overlay1').mouseenter(function(){
+        $('.portfolio-text').filter('#wrk1').show('slow')
+    }).mouseleave(function() {   
+    $('#wrk1').hide('slow');
+   
+       
+    });
+})
+
+$(document).ready(function(){
+    $('#overlay5').mouseenter(function(){
+        $('.portfolio-text').filter('#wrk5').show('slow')
+    }).mouseleave(function() {   
+    $('#wrk5').hide('slow');
+   
+       
+    });
+})
+
+$(document).ready(function(){
+    $('#overlay6').mouseenter(function(){
+        $('.portfolio-text').filter('#wrk6').show('slow')
+    }).mouseleave(function() {   
+    $('#wrk6').hide('slow');
+   
+       
+    });
+})
+
+$(document).ready(function(){
+    $('#overlay7').mouseenter(function(){
+        $('.portfolio-text').filter('#wrk7').show('slow')
+    }).mouseleave(function() {   
+    $('#wrk7').hide('slow');
+   
+       
+    });
+})
+
+$(document).ready(function(){
+    $('#overlay8').mouseenter(function(){
+        $('.portfolio-text').filter('#wrk8').show('slow')
+    }).mouseleave(function() {   
+    $('#wrk8').hide('slow');
+   
+       
+    });
+})
 
 
-    
+
